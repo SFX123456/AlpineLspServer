01c05117edf5b0edece044a35b46704bb7b088e6
frkri <57220519+frkri@users.noreply.github.com>
2024-04-08 09:53:25 +0200

    Small Fix

--- server/src/allRegex.ts
+++
@@ -1,8 +0,0 @@
-export const regexEndingOpeningTag =  />(?:[\r\n\s]*$|\s*<\/)/;
-export const regexOpeningTagHtml =   /<[a-z]+\s/;
-
-export const regexEndQuotationMarks = /(?<![\\=])"/g
-
-export const regexStartingAlpineExpression =  /(x-[a-zA-Z]*="|@[a-zA-Z]*=")/
-
-export const regexHighlightingSemantics =  /(?:x-([a-z:\.]+)|@([a-z-]+)[\.a-z-]*)="/g
--- server/src/methods/chainableOnAt.ts
+++
@@ -1,3 +0,0 @@
-export const chainableOnAt :string[] = ['stop', 'prevent', 'outside', 'window', 'document', 'once', 'debounce', 'throttle', 'self', 'camel', 'dot', 'passive', 'capture']
-export const chainableOnAtKeyboard : string[] = ['enter', 'shift', 'space', 'ctrl', 'cmd', 'meta', 'alt', 'up'
-    , 'down', 'left', 'right', 'escape', 'tab', 'caps-lock', 'equal', 'period', 'slash']
